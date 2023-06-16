c397e1eca9c8b0e0b6f7664b633bc0a50a9cfb60
Natan Feithlicher <feithlicher@gmail.com>
2023-06-16 11:47:54 +0300

    working on the moderation service

--- event-bus/index.js
+++ event-bus/index.js
@@ -8,13 +8,16 @@ app.use(bodyParser.json());
 app.post('/events', (req, res) => {
     const event = req.body;
 
-    axios.post('http://localhost:4000/events', event).catch((err) => {
+    axios.post('http://localhost:4000/events', event).catch((err) => { //posts
         console.log(err);
     });
-    axios.post('http://localhost:4001/events', event).catch((err) => {
+    axios.post('http://localhost:4001/events', event).catch((err) => { // comments
         console.log(err);
     });
-    axios.post('http://localhost:4002/events', event).catch((err) => {
+    axios.post('http://localhost:4002/events', event).catch((err) => { // query
+        console.log(err);
+    });
+    axios.post('http://localhost:4003/events', event).catch((err) => { // moderation
         console.log(err);
     });
 
